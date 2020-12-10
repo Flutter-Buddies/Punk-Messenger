6d888c8b58e4e0cb3d35f5d7ed59221db5924a6b
Jason Williams <williamsjacen007@gmail.com>
2020-12-09 21:51:36 -0500

    'Template'

---
+++ android/app/src/main/kotlin/com/example/punk_messenger/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.punk_messenger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'presentation/screens/welcome_screen.dart';
+import 'presentation/screens/login_screen.dart';
+import 'presentation/screens/registration_screen.dart';
+import 'presentation/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
