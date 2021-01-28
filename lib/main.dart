bc412199061a21d22e7b2d7a9c918c3f8a48f170
Hans Kokx <hans.d.kokx@gmail.com>
2021-01-28 13:07:23 -0500

    Added Cloud Firestore support
    Signed-off-by: Hans Kokx <hans.d.kokx@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,34 @@ import 'presentation/screens/welcome_screen.dart';
 import 'presentation/screens/login_screen.dart';
 import 'presentation/screens/registration_screen.dart';
 import 'presentation/screens/chat_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(FlashChat());
+
+Future<void> main() async {
+  await DotEnv.load();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  final FirebaseApp app = await Firebase.initializeApp(
+    name: 'db2',
+    options: new FirebaseOptions(
+      projectId: 'punkmessenger', // ? Is this correct?
+      appId: DotEnv.env['googleAppID'],
+      apiKey: DotEnv.env['apiKey'],
+      // databaseURL: DotEnv.env['databaseURL'],
+    ),
+  );
+
+  // TODO: Implement firestore https://firebase.flutter.dev/docs/firestore/usage/
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
